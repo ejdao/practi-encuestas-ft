f17616693ced0d1c446d2d5d127ac4f8e9a9539f
ejdao <ejdao2022@gmail.com>
2025-12-10 16:39:23 -0500

    :bento: NgComponents generado

---
+++ projects/components/testing/component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'tsd-testing',
+  templateUrl: './component.html',
+  styleUrl: './component.scss',
+})
+export class TestingComponent {}
